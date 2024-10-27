5995fbaaf3db8f3a03728865bedb91f8a2f84ff4
AmitBiradar1202 <amitbiradar1202@gmail.com>
2024-10-28 01:08:26 +0530

    l-update

--- server.js
+++ server.js
@@ -3,14 +3,12 @@ const express = require('express');
 const http = require('http');
 const cors = require('cors');
 const { Server } = require('socket.io');
-const {config}=require('dotenv')
-const app = express();
 
-config({ path: "./config.env" });
+const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "process.env.FRONTEND",
+    origin: "http://localhost:3000",
     methods: ["GET", "POST"]
   }
 });
@@ -41,5 +39,5 @@ io.on('connection', (socket) => {
   });
 });
 
-//const PORT = 5000;
-server.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
+const PORT = 5000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
