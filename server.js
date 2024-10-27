50921ba2e741c3ebc3f7e155461859950b5f2a9d
AmitBiradar1202 <amitbiradar1202@gmail.com>
2024-10-28 00:38:44 +0530

    config-Update

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const express = require('express');
 const http = require('http');
 const cors = require('cors');
 const { Server } = require('socket.io');
-
+const {config}=require("dotenv")
 const app = express();
 const server = http.createServer(app);
+
+config({ path: "./config.env" });
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "process.env.FRONTEND",
     methods: ["GET", "POST"]
   }
 });
@@ -16,6 +18,7 @@ const io = new Server(server, {
 app.use(cors());
 
 // When a client connects
+
 io.on('connection', (socket) => {
   console.log('A user connected');
 
@@ -39,5 +42,5 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = 5000;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+server.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
