437f699d50586b52cc90afe8710dccdad6675c64
AmitBiradar1202 <amitbiradar1202@gmail.com>
2024-10-28 00:15:11 +0530

    update

---
+++ server.js
@@ -0,0 +1,43 @@
+// server.js
+const express = require('express');
+const http = require('http');
+const cors = require('cors');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  }
+});
+
+app.use(cors());
+
+// When a client connects
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  // Join a room
+  socket.on('joinRoom', (data) => {
+    socket.join(data.room);
+    io.to(data.room).emit('message', {
+      user: 'System',
+      text: `${data.user} has joined the chat.`,
+    });
+  });
+
+  // Listen for chat messages
+  socket.on('chatMessage', (data) => {
+    io.to(data.room).emit('message', data);
+  });
+
+  // Handle disconnect
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+const PORT = 5000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
